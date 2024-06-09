7c8aa31e40471f2a11d1c13bdd5adf82c39b3b76
Strayneko <bayurendi10@gmail.com>
2024-06-09 15:30:27 +0700

    Feat: create decryption helper

--- src/helpers/encryptionHelper.ts
+++ src/helpers/encryptionHelper.ts
@@ -17,3 +17,25 @@ export const encryptionHelper = async (data: string) => {
     return null;
   }
 }
+
+export const decryptionHelper = async (data: string) => {
+  const secretKey = import.meta.env.VITE_SECRET_KEY;
+  try {
+    const decode = CryptoJS.enc.Base64.parse(data);
+    const ivParameterSpec = CryptoJS.enc.Utf8.parse(initialVector);
+    const secretKeySpec = CryptoJS.enc.Utf8.parse(secretKey);
+    const instance = CryptoJS.AES.decrypt(
+      { ciphertext: decode } as any,
+      secretKeySpec,
+      {
+        iv: ivParameterSpec,
+        padding: CryptoJS.pad.Pkcs7,
+        mode: CryptoJS.mode.CBC,
+      },
+    );
+    return instance.toString(CryptoJS.enc.Utf8);
+  } catch (e: any) {
+    console.error(e.message);
+    return null;
+  }
+}
