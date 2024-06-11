5e3d4b76feb15d7bc72cda2432bcaabe9f415db3
Strayneko <bayurendi10@gmail.com>
2024-06-12 01:28:52 +0700

    Feat: create auto attendance status updater

---
+++ src/views/Dashboard/DashboardView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import DataStatsOne from '@/components/DataStats/DataStatsOne.vue'
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
+
+</script>
+
+<template>
+  <DefaultLayout>
+    <div class="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+      <DataStatsOne />
+    </div>
+  </DefaultLayout>
+</template>
